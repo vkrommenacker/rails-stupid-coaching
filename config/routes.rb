5083dc7574184e02b3fe7e73916c89260d7a732f
Victoria Krommenacker <victoria.krmnckr@gmail.com>
2023-08-14 12:14:00 +0200

    rails stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'articles#index'
+  get 'ask', to: 'articles#ask'
+  get 'answer', to: 'articles#answer'
+# root "articles#index"
 end
